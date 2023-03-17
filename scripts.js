4340709ccd0dc7a0e15a587ae49d3bb5fd4a99ac
Matthew <105622101+m-wheeler-dev@users.noreply.github.com>
2023-03-16 18:06:15 -0700

    Add basic math functions

--- scripts.js
+++ scripts.js
@@ -6,3 +6,20 @@ const decimalBtn = document.querySelector("#deci-btn");
 const equalsBtn = document.querySelector("#eq-btn");
 const prevDisplay = document.querySelector(".upper-display");
 const currDisplay = document.querySelector(".lower-display");
+
+// BASIC MATH FUNCTIONS
+const add = (x, y) => {
+	return x + y;
+};
+
+const subtract = (x, y) => {
+	return x - y;
+};
+
+const multiply = (x, y) => {
+	return x * y;
+};
+
+const divide = (x, y) => {
+	return x / y;
+};
