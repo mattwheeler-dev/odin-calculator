3ab35864707d9e169e0175e542a2d08f4d99d5ee
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-03-09 15:14:22 -0700

    finish styling and icons; refactor js for clarity;

--- scripts.js
+++ scripts.js
@@ -1,118 +1,119 @@
-const numBtns = document.querySelectorAll("[data-number]");
-const operatorBtns = document.querySelectorAll("[data-operator]");
-const clearBtn = document.getElementById("clear-btn");
-const deleteBtn = document.querySelector("#del-btn");
-const decimalBtn = document.querySelector("#deci-btn");
-const equalsBtn = document.querySelector("#eq-btn");
-const prevDisplay = document.querySelector(".upper-display");
-const currDisplay = document.querySelector(".lower-display");
+const numBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const clearBtn = document.querySelector('#clear-btn');
+const deleteBtn = document.querySelector('#del-btn');
+const decimalBtn = document.querySelector('#deci-btn');
+const equalsBtn = document.querySelector('#eq-btn');
+const prevDisplay = document.querySelector('#upper-display');
+const currDisplay = document.querySelector('#lower-display');
 let currOperation = null;
-let firstOperand = "";
-let secondOperand = "";
+let firstOperand = '';
+let secondOperand = '';
 
-// ADD EVENT LISTENERS FOR NUMS
-numBtns.forEach((numBtn) => {
-	numBtn.addEventListener("click", () => displayNums(numBtn.textContent));
-});
-
-// ADD EVENT LISTENERS FOR OPERATORS
-operatorBtns.forEach((opBtn) => {
-	opBtn.addEventListener("click", () => setOperation(opBtn.textContent));
-});
-
-// ADD EVENT LISTENERS FOR DECIMAL, CLEAR, DELETE, & EQUALS
-decimalBtn.addEventListener("click", addDecimal);
-clearBtn.addEventListener("click", clear);
-deleteBtn.addEventListener("click", deleteLast);
-equalsBtn.addEventListener("click", evaluate);
-
-// DISPLAY NUMBER FUNCTION
-function displayNums(num) {
-	if (currDisplay.textContent === "0") {
-		currDisplay.textContent = "";
+const displayNums = (num) => {
+	if (currDisplay.textContent === '0') {
+		currDisplay.textContent = '';
 		currDisplay.textContent += num;
 	} else {
 		currDisplay.textContent += num;
 	}
-}
+};
 
-// ADD DECIMAL FUNCTION
-function addDecimal() {
-	if (currDisplay.textContent.includes(".")) {
+const addDecimal = () => {
+	if (currDisplay.textContent.includes('.')) {
 		return;
 	} else {
-		currDisplay.textContent += ".";
+		currDisplay.textContent += '.';
 	}
-}
+};
 
-// SET OPERATION FUNCTION
-function setOperation(operator) {
+const setOperation = (operator) => {
 	firstOperand = currDisplay.textContent;
-	currDisplay.textContent = "0";
+	currDisplay.textContent = '0';
 	currOperation = operator;
 	prevDisplay.textContent = `${firstOperand} ${currOperation}`;
-}
+};
 
-// CLEAR FUNCTION
-function clear() {
-	prevDisplay.textContent = "0";
-	currDisplay.textContent = "0";
-}
+const clear = () => {
+	prevDisplay.textContent = '0';
+	currDisplay.textContent = '0';
+};
 
-// DELETE LAST FUNCTION
-function deleteLast() {
+const deleteLast = () => {
+	if (currDisplay.textContent.length === 1) {
+		currDisplay.textContent = '0';
+		return;
+	}
 	currDisplay.textContent = currDisplay.textContent.toString().slice(0, -1);
-}
+};
 
-// BASIC MATH FUNCTIONS
-function add(x, y) {
+const add = (x, y) => {
 	return x + y;
-}
+};
 
-function subtract(x, y) {
+const subtract = (x, y) => {
 	return x - y;
-}
+};
 
-function multiply(x, y) {
+const multiply = (x, y) => {
 	return x * y;
-}
+};
 
-function divide(x, y) {
+const divide = (x, y) => {
 	return x / y;
-}
+};
 
-// OPERATE / CALCULATE FUNCTION
-function operate(num1, num2, operator) {
+const operate = (num1, num2, operator) => {
 	num1 = Number(num1);
 	num2 = Number(num2);
 
 	switch (operator) {
-		case "+":
+		case '+':
 			return add(num1, num2);
-		case "-":
+		case '-':
 			return subtract(num1, num2);
-		case "*":
+		case '*':
 			return multiply(num1, num2);
-		case "/":
+		case '/':
 			return divide(num1, num2);
 		default:
 			return null;
 	}
-}
+};
 
-// EVALUATE THE PROBLEM & CALL OPERATE IF VALID
-function evaluate() {
+const evaluate = () => {
 	if (currOperation === null) {
 		return;
-	} else if (currOperation === "/" && currDisplay.textContent === "0") {
-		alert(`Don't try dividing by 0! It leads only to heartbreak...`);
+	} else if (currOperation === '/' && currDisplay.textContent === '0') {
+		alert(
+			'Error: Dividing by 0 can create black holes... please refrain from sending our universe to the void...',
+		);
 		return;
 	} else {
 		secondOperand = currDisplay.textContent;
 		currDisplay.textContent =
-			Math.round(operate(firstOperand, secondOperand, currOperation) * 100) /
-			100;
+			Math.round(
+				operate(firstOperand, secondOperand, currOperation) * 100,
+			) / 100;
 		prevDisplay.textContent = `${firstOperand} ${currOperation} ${secondOperand} =`;
 		currOperation = null;
 	}
-}
+};
+
+// Event listeners
+numBtns.forEach((numBtn) => {
+	numBtn.addEventListener('click', () => displayNums(numBtn.textContent));
+});
+
+operatorBtns.forEach((opBtn) => {
+	opBtn.addEventListener('click', () => setOperation(opBtn.textContent));
+});
+
+decimalBtn.addEventListener('click', addDecimal);
+clearBtn.addEventListener('click', clear);
+deleteBtn.addEventListener('click', deleteLast);
+equalsBtn.addEventListener('click', evaluate);
+
+// Footer
+const footer = document.querySelector('footer p');
+footer.textContent = `© ${new Date().getFullYear()} mattwheeler-dev`;
